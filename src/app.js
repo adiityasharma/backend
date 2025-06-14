2bd8915fe199bdc638fb0f5e6f3cc047842ed2f9
Aditya Sharma <adityasharma626367@gmail.com>
2025-06-14 20:36:14 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+//cookieParser: to access cookies of the browser and can do crud
+import cookieParser from "cookie-parser"; 
+import cors from "cors"
+
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import { app } from "./app.js"
 import connectDB from "./db/db.js"
 import 'dotenv/config'
 
 
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`Server is started..`)
+  })
+})
+.catch((err)=>{
+  console.log("MongoDB connection failed. Error", err)
+})
